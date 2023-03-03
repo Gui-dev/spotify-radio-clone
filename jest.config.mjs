8fdac2ccecf5371ea014074ce1700cc88b99cb1c
Gui-dev <adrianon10@gmail.com>
2023-03-03 14:25:37 -0300

    chore: add jest config

---
+++ jest.config.mjs
@@ -0,0 +1,59 @@
+const defaultConfig = {
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: [
+    'text',
+    'lcov'
+  ],
+  coverageThreshold: {
+    global: {
+      branch: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  maxWorkers: '50%',
+  watchPathIgnorePatterns: [
+    'node_modules'
+  ],
+  transformIgnorePatterns: [
+    'node_modules'
+  ]
+}
+
+export default {
+  projects: [
+    {
+      ...defaultConfig,
+      testEnvironment: 'node',
+      displayName: 'backend',
+      collectCoverageFrom: [
+        'server/src/',
+        '!server/src/index.js'
+      ],
+      transformIgnorePatterns: [
+        ...defaultConfig.transformIgnorePatterns,
+        'web/public'
+      ],
+      testMatch: [
+        '**/server/src/__tests__/*.spec.js'
+      ]
+    },
+    {
+      ...defaultConfig,
+      testEnvironment: 'jsdom',
+      displayName: 'frontend',
+      collectCoverageFrom: [
+        'web/public',
+      ],
+      transformIgnorePatterns: [
+        ...defaultConfig.transformIgnorePatterns,
+        'web/public'
+      ],
+      testMatch: [
+        '**/web/public/__tests__/*.spec.js'
+      ]
+    }
+  ]
+}
