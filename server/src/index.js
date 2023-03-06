33f21a32918477e2057695c16925f1547aa0599d
Gui-dev <adrianon10@gmail.com>
2023-03-06 04:19:59 -0300

    refactor: remove config

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,7 @@
 import { server } from './server.js'
 import { logger } from './util/logger.js'
-import config from './config.js'
 
 const PORT = 3333 || process.env.PORT
-
-logger.info(config)
 
 server.listen(PORT)
   .on('listening', () => logger.info(`Server running on http://localhost:${PORT}`))
