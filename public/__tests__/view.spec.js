a8e3af03545dbfb00a0da83ee8d746d84f97f7c8
Gui-dev <adrianon10@gmail.com>
2023-03-14 04:24:08 -0300

    test: it #changeCommandBtnsVisibility - given hide=true it should add unassigned class and reset onclick

--- public/__tests__/view.spec.js
+++ public/__tests__/view.spec.js
@@ -3,21 +3,21 @@ import { JSDOM } from 'jsdom'
 
 import { View } from './../controller/src/view.js'
 
-describe('#View - test suite for presentation layer', () => {
+describe('#View - test suite for presentation layer ', () => {
   const dom = new JSDOM()
   global.document = dom.window.document
   global.window = dom.window
 
-  const makeButtonElement = (
-    { text, classList } =
-      {
-        test: '',
-        classList: {
-          add: jest.fn(),
-          remove: jest.fn()
-        }
+  function makeBtnElement ({
+    text,
+    classList
+  } = {
+      text: '',
+      classList: {
+        add: jest.fn(),
+        remove: jest.fn(),
       }
-  ) => {
+    }) {
     return {
       onclick: jest.fn(),
       classList,
@@ -28,23 +28,26 @@ describe('#View - test suite for presentation layer', () => {
   beforeEach(() => {
     jest.resetAllMocks()
     jest.clearAllMocks()
-
     jest.spyOn(
       document,
-      document.getElementById.name
-    ).mockReturnValue([makeButtonElement()])
+      "getElementById"
+    ).mockReturnValue(makeBtnElement())
   })
 
-  it('#changeCommandButtonsVisibility - given hide=true it should add unassigned class and reset onclick', async () => {
+  it('#changeCommandBtnsVisibility - given hide=true it should add unassigned class and reset onclick', () => {
     const view = new View()
-    const button = makeButtonElement()
+    const btn = makeBtnElement()
     jest.spyOn(
       document,
-      document.querySelectorAll.name
-    ).mockReturnValue([button])
+      "querySelectorAll"
+    ).mockReturnValue([btn])
+
     view.changeCommandButtonsVisibility()
 
-    expect(button.classList.add).toHaveBeenCalledWith('unassigned')
+    expect(btn.classList.add).toHaveBeenCalledWith('unassigned')
+    expect(btn.onclick.name).toStrictEqual('onClickReset')
+    expect(() => btn.onclick()).not.toThrow()
+
   })
   it.todo('#changeCommandButtonsVisibility - given hide=false it should remove unassigned class and reset onclick')
   it.todo('#onLoad - given hide=true it should add unassigned class and reset onclick')
